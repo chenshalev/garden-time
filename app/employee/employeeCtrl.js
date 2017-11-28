6cda2f809ae3ac4660fcf122c75309e2cccb9e4d
chenshalev <chensha07@gmail.com>
2017-11-28 20:18:06 +0200

    tasks of employee-basic show buy working

--- app/employee/employeeCtrl.js
+++ app/employee/employeeCtrl.js
@@ -1,7 +1,28 @@
-gardenApp.controller("employeeCtrl", function($scope, $log, $http, $location,activeUser) {
-    $scope.test = "ewewew";           
-    $scope.user=activeUser.get();
-    
+gardenApp.controller("employeeCtrl", function ($scope, $log, $http, $location, activeUser, tasks) {
 
+    if (!activeUser.isLoggedIn()) {
+        $location.path("/");
+        return;
+    }
+    $scope.user = activeUser.get();
 
- });
+    // http call to get data from json
+
+    $http.get("app/model/data/json/tasks.json").then(function mySuccess(response) {
+        // Updating the service with the data
+        tasks.load(response.data);
+        // Getting the data from the service
+        taskArr=tasks.getAll();
+        tasktemp=[];
+        for (var index = 0; index < taskArr.length; index++) {
+            if (taskArr[index].Employee === $scope.user.firstName) {
+                tasktemp.push(taskArr[index]);
+   
+            }
+        }
+        $scope.taskArr=tasktemp;
+    }, function myError(response) {
+        alert("error" + JSON.stringify(response.status));
+    });
+
+});
---
+++ app/model/tasks.js
@@ -0,0 +1,59 @@
+// Shared Recipe Constructor
+gardenApp.factory("Task", function(){
+    function Task(plainObject) {
+        this.activity= plainObject.activity;
+        this.location = plainObject.location;
+        this.dueDate = plainObject.dueDate;
+        this.Employee = plainObject.Employee;
+        this.imageUrl = plainObject.imageUrl;
+    };
+
+
+    return Task;
+});
+
+
+
+gardenApp.factory("tasks", function(Task) {
+    var taskArr = [];
+
+    var add = function(task) {
+        taskArr.push(task);
+    }
+
+    var update = function(index, task) {
+        taskArr[index] = task;
+    }
+
+    var remove = function(index) {
+        taskArr.splice(index, 1);
+    }
+
+    var load = function(taskPlainObjectArr) {
+        for (var i = 0; i < taskPlainObjectArr.length; i++) {
+            taskArr.push(new Task(taskPlainObjectArr[i]))
+        }
+    }
+
+    var getAll = function() {
+        return taskArr;
+    }
+
+    var get = function(index) {
+        return taskArr[index];
+    }
+
+    var removeAll = function() {
+        taskArr = [];
+    }
+    
+    return {
+        add: add,
+        update: update,
+        remove: remove,
+        load: load,
+        getAll: getAll,
+        get: get,
+       removeAll: removeAll
+    }
+})
