a6787067a5570a4be941af5493cd6f79fd455833
chenshalev <chensha07@gmail.com>
2017-11-29 15:39:41 +0200

    main window-for all -working

--- app/manager/managerMainCtrl.js
+++ app/manager/managerMainCtrl.js
@@ -1,8 +1,18 @@
 
 gardenApp.controller("managerMainCtrl", function($scope, $log, $http, $location,activeUser) {
     $scope.user=activeUser.get();
+     
+            if (!activeUser.isLoggedIn()) {
+                $location.path("/");
+                return;
+            }
+            $scope.user = activeUser.get();
 
-    
-
-
+            $http.get("app/model/data/json/activity.json").then(function mySuccess(response) {
+                // Updating the service with the data
+                $scope.activityArr=response.data;
+                  }, function myError(response) {
+                alert("error" + JSON.stringify(response.status));
+            });
+        
  });
