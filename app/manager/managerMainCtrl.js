7cf2fee38d0f482afbc5c62e51b8d6e0449884de
chenshalev <chensha07@gmail.com>
2017-11-29 15:51:46 +0200

    drill down from main to activities

--- app/manager/managerMainCtrl.js
+++ app/manager/managerMainCtrl.js
@@ -1,18 +1,23 @@
 
-gardenApp.controller("managerMainCtrl", function($scope, $log, $http, $location,activeUser) {
-    $scope.user=activeUser.get();
-     
-            if (!activeUser.isLoggedIn()) {
-                $location.path("/");
-                return;
-            }
-            $scope.user = activeUser.get();
+gardenApp.controller("managerMainCtrl", function ($scope, $log, $http, $location, activeUser) {
+    $scope.user = activeUser.get();
 
-            $http.get("app/model/data/json/activity.json").then(function mySuccess(response) {
-                // Updating the service with the data
-                $scope.activityArr=response.data;
-                  }, function myError(response) {
-                alert("error" + JSON.stringify(response.status));
-            });
-        
- });
+    if (!activeUser.isLoggedIn()) {
+        $location.path("/");
+        return;
+    }
+    $scope.user = activeUser.get();
+
+    $http.get("app/model/data/json/activity.json").then(function mySuccess(response) {
+        // Updating the service with the data
+        $scope.activityArr = response.data;
+    }, function myError(response) {
+        alert("error" + JSON.stringify(response.status));
+    });
+
+    $scope.openActivity = function (activity) {
+        $location.path("/" + activity.window);
+    }
+
+
+});
