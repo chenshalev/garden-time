16707ac6e8ee889da84d08da2a28125be4089f92
chenshalev <chensha07@gmail.com>
2017-11-29 16:06:02 +0200

    enter each window activity-working!

--- app/app.js
+++ app/app.js
@@ -14,7 +14,28 @@ gardenApp.config(function ($routeProvider) {
             templateUrl: "app/employee/employeeMain.html",
             controller: "employeeCtrl"
         })
-
+        .when("/Watering", {
+            templateUrl: "app/activities/Watering.html",
+         })
+         .when("/Fertilization", {
+            templateUrl: "app/activities/Fertilization.html",
+         })
+      .when("/Trimming", {
+            templateUrl: "app/activities/Trimming.html",
+         })
+         .when("/Fruit", {
+            templateUrl: "app/activities/Fruit.html",
+         })
+         .when("/Planting", {
+            templateUrl: "app/activities/Planting.html",
+         })
+         .when("/Settings", {
+            templateUrl: "app/activities/Settings.html",
+         })
+         .when("/Tasks", {
+            templateUrl: "app/activities/Tasks.html",
+         })
+ 
 })
     /*
   gardenApp.controller("TestCtrl", function ($scope, User, activeUser) {
