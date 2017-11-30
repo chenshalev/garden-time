a5597121e968166212696244a75a3d722cccad4a
Orly Sluszny <orly.sluszny@gmail.com>
2017-11-30 14:08:10 +0200

    newtask location: plant employee

--- app/newtask/newtaskCtrl.js
+++ app/newtask/newtaskCtrl.js
@@ -11,5 +11,23 @@ gardenApp.controller("newTaskCtrl", function ($scope, $log, $http, $location, ac
     }, function myError(response) {
         alert("error" + JSON.stringify(response.status));
     });
-
+    $http.get("app/model/data/json/location.json").then(function mySuccess(response) {
+        // Updating the service with the data
+        $scope.locationArr = response.data; 
+    }, function myError(response) {
+        alert("error" + JSON.stringify(response.status));
+    });
+    $http.get("app/model/data/json/plants.json").then(function mySuccess(response) {
+        // Updating the service with the data
+        $scope.plantsArr = response.data; 
+    }, function myError(response) {
+        alert("error" + JSON.stringify(response.status));
+    });
+    $http.get("app/model/data/json/users.json").then(function mySuccess(response) {
+        // Updating the service with the data
+        $scope.employeeArr = response.data; 
+    }, function myError(response) {
+        alert("error" + JSON.stringify(response.status));
+    });
+    
 });
