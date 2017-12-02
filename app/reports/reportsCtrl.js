7d9f31f745b1f63ab1f56c42d0ceb6b87aa1c88d
chenshalev <chensha07@gmail.com>
2017-12-02 20:15:25 +0200

    reports

--- app/reports/reportsCtrl.js
+++ app/reports/reportsCtrl.js
@@ -6,5 +6,14 @@ gardenApp.controller("reportsCtrl", function ($scope, $log, $http, $location, ac
         }
         $scope.user = activeUser.get();
     
-     
+        $http.get("app/model/data/json/tasks.json").then(function mySuccess(response) {
+            // Updating the service with the data
+            tasks.load(response.data);
+            // Getting the data from the service
+            taskArr=tasks.getAll();
+            $scope.taskArr=taskArr;
+        }, function myError(response) {
+            alert("error" + JSON.stringify(response.status));
+        });
+        
     });
