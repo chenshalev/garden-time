3de74cb7abec8c60c9e76bffa0717fb830f85d4a
Orly Sluszny <orly.sluszny@gmail.com>
2017-12-04 20:31:52 +0200

    undo footer links

--- app/app.js
+++ app/app.js
@@ -81,7 +81,6 @@ gardenApp.config(function($routeProvider) {
             templateUrl: "app/alert/alert.html",
             controller: "alertCtrl"
         })
-<<<<<<< HEAD
         .when("/contact", {
             templateUrl: "app/activities/contact.html",
         })
@@ -91,7 +90,3 @@ gardenApp.config(function($routeProvider) {
 
 
 })
-=======
- 
-    })    
->>>>>>> 117a64b0a83e04d9d10b556e2c1048e2f48c4658
