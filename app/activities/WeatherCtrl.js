8996453f6411467bc58754c9ce783264598723b6
chenshalev <chensha07@gmail.com>
2017-12-04 21:54:29 +0200

    weather  working

--- app/activities/WeatherCtrl.js
+++ app/activities/WeatherCtrl.js
@@ -1,21 +1,37 @@
-gardenApp.controller("WeatherCtrl", function ($scope, $log, $http, $location, activeUser, tasks, $route, Task,activeTask) {
- 
+gardenApp.controller("WeatherCtrl", function ($scope, $log, $http, $location, activeUser, tasks, $route, Task, activeTask) {
+
     if (!activeUser.isLoggedIn()) {
         $location.path("/");
         return;
     }
     $scope.user = activeUser.get();
-
     if (navigator.geolocation) {
-        $scope.place="dsfdfsfd";
-    }
-    //navigator.geolocation.getCurrentPosition(function(position) {
-       // var golink = " https://fcc-weather-api.glitch.me/api/current?lat="+position.coords.latitude+"&lon="+position.coords.longitude;
-   //$.getJSON(golink, function(result){
+        var options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        };
 
-    //place=result.place;
-    //image=result.weather[0].main;
-    //   $("#data").append("<h1>"+result.weather[0].main+"</h1>");
-    //  $("#data").append("<h1>"+result.main.temp+"*C</h1>");
-    //  $("#data").append("<h2>Humidity:"+result.main.humidity+"</h2>");
-   })
+        function success(pos) {
+            var crd = pos.coords;
+            var golink = "https://fcc-weather-api.glitch.me/api/current?lat=" + crd.latitude + "&lon=" + crd.longitude;
+            $http.get(golink).then(function mySuccess(response) {
+                var result = response.data;
+                $scope.place = result.name;
+                $scope.image = result.weather[0].icon;
+                $scope.weather = result.weather[0].main;
+                $scope.temp = result.main.temp ;
+                $scope.temp=($scope.temp-32) * 5 / 9;
+                $scope.temp=Math.round($scope.temp);
+  
+            });
+        }
+        function error(err) {
+            console.warn(`ERROR(${err.code}): ${err.message}`);
+        };
+
+        navigator.geolocation.getCurrentPosition(success, error, options);
+    }
+
+})
+
