5db9ec41194b797210aa8b8ce534e461e058815f
Orly Sluszny <orly.sluszny@gmail.com>
2017-12-06 09:29:01 +0200

    home.html text size

--- app/contact/contactCtrl.js
+++ app/contact/contactCtrl.js
@@ -1,8 +1,8 @@
 gardenApp.controller("contactCtrl", function ($scope, $log, $http, $location, activeUser) {
     
-        if (!activeUser.isLoggedIn()) {
+       /* if (!activeUser.isLoggedIn()) {
             $location.path("/");
             return;
-        }
+        }*/
         $scope.user = activeUser.get();
     });
